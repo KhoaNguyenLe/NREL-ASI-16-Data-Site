b3e0d2b96cf53efd680f4c176cbb1b96d9db0ca9
Khoa Le <khoanguyenle2007@gmail.com>
2025-11-08 17:49:13 -0600

    new object for data + fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
-interface Fileinfo{
+interface FileInfo{
   label: string;
   directoryLabel?: string;
   downloadURL: string;
 }
 
 export default function Home() {
+
+  const [files, setFiles] = useState<FileInfo[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData(){
+      try{
+        const res = await fetch(
+          'link'
+        );
+        const data = await res.json();
+        const fileList = data.data.latestVersion.files.map((f:any) =>({
+          label: f.label,
+          directoryLabel: f.directoryLabel,
+          downloadURL: f.dataFile.downloadURL,
+        }));
+        setFiles(fileList);
+      } catch(err){
+        console.error(err);
+      } finally{
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
       <main className="min-h-screen bg-gray-50 text-gray-800">
         <div>
