7d03fd8d8b4b82cc3108d74162549b7140398ead
Khoa Le <khoanguyenle2007@gmail.com>
2025-11-08 17:39:20 -0600

    interface + react imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface Fileinfo{
+  label: string;
+  directoryLabel?: string;
+  downloadURL: string;
+}
 
 export default function Home() {
   return (
       <main className="min-h-screen bg-gray-50 text-gray-800">
         <div>
-          
+          <p>
+            
+          </p>
         </div>
       </main>
         
